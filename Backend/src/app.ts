a034ef5fbc03464518c7c2e9f323f1d9bdab27d1
Tushar Shaw <tusharshaw961@gmail.com>
2026-04-08 19:16:37 +0530

    Task completed

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://ai-battle-arena-1-o1fh.onrender.com"
 }));
 
 
