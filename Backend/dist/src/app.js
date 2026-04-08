4cd757494672ba926c43d4e506ea6425dc8bf339
Tushar Shaw <tusharshaw961@gmail.com>
2026-04-08 19:30:14 +0530

    Task completed.

--- Backend/dist/src/app.js
+++ Backend/dist/src/app.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-battle-arena-1-o1fh.onrender.com"
 }));
 app.get("/health", (req, res) => {
     res.status(200).json({ status: "ok" });
